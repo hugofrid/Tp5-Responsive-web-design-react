05440dfa08eed31eaa934a3d595d3e02b20a310d
hugofrid <hugofrid2@gmail.com>
2019-02-18 11:03:16 +0100

    preparation fichier

--- tp5-react-part/src/App.js
+++ tp5-react-part/src/App.js
@@ -3,20 +3,22 @@ import Header from './container/Header';
 import SideMenu from './container/SideMenu';
 import Artist from './component/Artist';
 import AlbumGrid from './container/AlbumGrid';
+import {Container, Col, Row} from 'reactstrap';
+
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Header />
         <SideMenu />
         <main>
           <Artist />
           <AlbumGrid />
         </main>
-      </div>
+        </Container>
     );
   }
 }
